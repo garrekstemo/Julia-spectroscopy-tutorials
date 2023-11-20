c1a9bdec20f9bbf2f0c9f55b182172210cbab089
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2023-11-20 18:51:34 +0900

    Generate new data

--- src/generate_data.jl
+++ src/generate_data.jl
@@ -2,30 +2,29 @@ using DelimitedFiles
 using GLMakie
 
 function cavity_mode_energy(θs, E_0, n)
-    E_c = Float64[]
-    for θ_i in θs
-        E_ci = E_0 / sqrt(1 - sin(θ_i)^2 / n^2)
-        push!(E_c, E_ci)
+    E_c = zeros(length(θs))
+    for i in eachindex(θs)
+        E_c_i = E_0 / sqrt(1 - sin(θs[i])^2 / n^2)
+        E_c[i] = E_c_i
     end
     return E_c
 end
 
 function polariton_branches(θs, E_v, E_0, n, Ω, branch = 1)
-    E = Float64[]
+    E = zeros(length(θs))
     E_c = cavity_mode_energy(θs, E_0, n)
 
     if branch == 1
         for i in eachindex(θs)
             E_i = 0.5 * (E_v + E_c[i] - sqrt(Ω^2 + (E_c[i] - E_v)^2)) 
-            push!(E, E_i)
+            E[i] = E_i
         end
     elseif branch == 2
         for i in eachindex(θs)
             E_i = 0.5 * (E_v + E_c[i] + sqrt(Ω^2 + (E_c[i] - E_v)^2))
-            push!(E, E_i)
+            E[i] = E_i
         end
     end
-    
     return E
 end
 
@@ -34,10 +33,11 @@ end
 # Generate fake data here by setting n, E_v, E_0, and Ω,
 # which students must find by writing a fitting procedure.
 
+E_v, E_0, n, Ω = [1953.7, 1900.1, 1.38, 94.5]
 θs = range(0.0, 40, length = 10)
 θsrad = θs .* π/180.0
-LP = polariton_branches(θsrad, E_v, E_0, n, Ω, 1) .+ 1 * randn(length(θsrad))
-UP = polariton_branches(θsrad, E_v, E_0, n, Ω, 2) .+ 3.5 * randn(length(θsrad))
+LP = polariton_branches(θsrad, E_v, E_0, n, Ω, 1) .+ 1.3 * randn(length(θsrad))
+UP = polariton_branches(θsrad, E_v, E_0, n, Ω, 2) .+ 3.7 * randn(length(θsrad))
 
 fig = Figure()
 ax = Axis(fig[1, 1])
@@ -48,6 +48,6 @@ fig
 
 
 ##
-# open("data/generated_polariton_data.csv", "w") do io
-#     writedlm(io, [θs LP UP], ',')
-# end
+open("data/generated_polariton_data.csv", "w") do io
+    writedlm(io, [θs LP UP], ',')
+end
